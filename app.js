d4c3c089b0b2797cd5cc5a81940f6e1baea67c2e
Professor José de Assis <josedeassisfilho@gmail.com>
2025-02-06 22:18:05 -0300

    app

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * APP PWA - Etanol x Gasolina
+ * @author Professor José de Assis
+ * @version 2.0
+ */
+
+function calcular() {
+    //capturar os valores das caixas de input
+    let gasolina = document.getElementById('gasolina').value
+    let etanol = document.getElementById('etanol').value
+    let kmlGasolina = document.getElementById('kmGasolina').value
+    let kmlEtanol = document.getElementById('kmEtanol').value
+    //cálculo da vantagem
+    if (etanol < (kmlEtanol / kmlGasolina) * gasolina) {
+        document.getElementById('status').src = "img/etanol.png"
+    } else {
+        document.getElementById('status').src = "img/gasolina.png"
+    }
+}
