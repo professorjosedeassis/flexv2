a8edda387856525e6dfcea8066337730aa0d4fee
Professor José de Assis <josedeassisfilho@gmail.com>
2025-02-11 22:08:18 -0300

    service worker

--- app.js
+++ app.js
@@ -4,6 +4,19 @@
  * @version 2.0
  */
 
+//===========================================
+//Registro do service worker
+
+// se o navegador de internet suportar este recurso
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(() => {
+            console.log("Service worker registrado!")
+        })
+}
+//===========================================
+
 function calcular() {
     //capturar os valores das caixas de input
     let gasolina = document.getElementById('gasolina').value
---
+++ sw.js
@@ -0,0 +1,39 @@
+/**
+ * Service worker
+ * @author Professor José de Assis
+ */
+
+// Instalação (cache "armazenamento local")
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('static')
+            .then((cache) => {
+                cache.add('/flexv2/')
+                cache.add('/flexv2/index.html')
+                cache.add('/flexv2/style.css')
+                cache.add('/flexv2/app.js')
+                cache.add('/flexv2/img/flex.png')
+                cache.add('/flexv2/img/calcflex.png')
+                cache.add('/flexv2/img/etanol.png')
+                cache.add('/flexv2/img/gasolina.png')
+            })
+    )
+})
+// Ativação
+self.addEventListener('activate', (event) => {
+    console.log("Ativando o service worker...", event)
+    return self.clients.claim()
+})
+// Interceptação (solicitações https servindo em cache quando off-line)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response
+                } else {
+                    return fetch(event.request)
+                }
+            })
+    )
+})
